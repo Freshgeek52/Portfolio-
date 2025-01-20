b9731f7955e0aac0731545d09ec6610485496094
Freshgeek52 <freshgeek252@gmail.com>
2025-01-20 03:25:42 +0200

    updating About page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ menuIcon.onclick = ()=>{
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
 }
-
+ 
 //Scroll section
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
